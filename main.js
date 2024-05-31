00644443152e139baaf036cafa778bc4d1fc37fb
AsheelAhmedSiddiqui <asheelahmedsiddiqui@gmail.com>
2024-05-31 20:53:56 +0500

    JS-Function-Assignment-5

---
+++ main.js
@@ -0,0 +1,6 @@
+function sqrt(num) {
+  return (document.getElementById("para").innerText = Math.sqrt(num));
+}
+function sqrt1(num) {
+  return (document.getElementById("para").innerText = num * num);
+}
